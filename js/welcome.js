9acbaf7faf832e9a0c76895eaefcafc35e7b7fc9
imran <imransunday@gmail.com>
2018-02-18 16:38:14 +0100

    moved welcome.html to welcome.jsp to make the code reusable like the header.jsp and footer.jsp

---
+++ js/welcome.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+
+    //register the team
+    $("#rc-form").submit(function(e) {
+
+        var url = "/jsp/register.jsp"; //register the candidates
+
+        $.ajax({
+               type: "POST", //method POST
+               url: url,
+               data: $("#rc-form").serialize(), // serializes the form's elements.
+               success: function(data)
+               {
+                   console.log("Response:" + data); // show response
+                   var response = JSON.parse(data);
+                   console.log("Message" + response.message);
+                   if (response.status == 0) {
+                        var redirectUrl = response.redirectUrl;
+                        var delay = 1000;
+                        setTimeout(function(){ window.location = redirectUrl; }, delay);
+                   }
+               },
+               error: function(xhr, textStatus, errorThrown) {
+                    alert('Error!  Status = ' + xhr.status);
+                }
+        });
+        e.preventDefault(); // avoid to execute the actual submit of the form.
+    });
+});
